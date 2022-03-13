2a6745bdcb90eaf1836bfd98a49f9ea9f1254d88
Mark Jue <markjue@zettatips.com>
2022-03-13 12:53:26 +0800

    Update Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ const Footer = () => (
     <div className={styles.container}>
       Built with <a href="https://contentful.com/">Contentful</a> and{' '}
       <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      <a href="https://github.com/zettatips/gatsby-blog">Developed by Zetta Tips</a>
     </div>
   </Container>
 )
